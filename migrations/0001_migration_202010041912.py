d3b7241521b92875581dcac48f5672fa84cd2ddd
mickorivera <18693715+mickorivera@users.noreply.github.com>
2020-10-04 19:13:55 +0800

    create user migration file

---
+++ migrations/0001_migration_202010041912.py
@@ -0,0 +1,23 @@
+# auto-generated snapshot
+from peewee import *
+import datetime
+import peewee
+
+
+snapshot = Snapshot()
+
+
+@snapshot.append
+class UserModel(peewee.Model):
+    username = CharField(index=True, max_length=16, unique=True)
+    email_address = CharField(max_length=256, unique=True)
+    salt = BlobField()
+    key = BlobField()
+    role = CharField(default='MEMBER', max_length=255)
+    date_created = DateTimeField(null=True)
+    date_updated = DateTimeField(null=True)
+    is_deleted = BooleanField(default=False)
+    class Meta:
+        table_name = "users"
+
+
